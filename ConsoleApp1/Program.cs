e6282b96422c811e490af6ea2458d3459b3c0dc9
ispan <ispan@DESKTOP-PAKSETB>
2022-10-28 16:29:19 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Member[] items = {
+                new Member{Name="A1",Height=195,DateOfBirth=new DateTime(1999,1,2) },
+                new Member{Name="A2",Height=180,DateOfBirth=new DateTime (1992,2,5)},
+                new Member{Name="A3",Height=185,DateOfBirth=new DateTime (1994,1,3)}
+                
+            };
+            //Array.Sort(items);
+
+            //IComparer<Member> comparer = new CompareByNameDESC();
+            //IComparer<Member> comparer2 = new CompareByNameASC();
+
+            //IComparer<Member> comparer3 = new CompareByHeightDESC();
+            //IComparer<Member> comparer4 = new CompareByHeightASC();
+            //comparer=new CompareByNameASC
+            Array.Sort(items,Member.按姓名遞增排序);
+
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+   
+    internal class CompareByHeightASC : IComparer<Member>
+    {
+        public int Compare(Member x, Member y)
+        {
+            return x.Height.CompareTo(y.Height);
+        }
+    }
+
+    public class CompareByNameASC:IComparer<Member>
+    {
+        public int Compare (Member x,Member y)
+        {
+            return x.Name.CompareTo(y.Name);
+        }
+    }
+    public class CompareByNameDESC: IComparer<Member>
+    {
+        public int Compare(Member x, Member y)
+        {
+            return y.Name.CompareTo(x.Name);
+        }
+    }
+
+
+    public class Member : IComparable<Member>
+    {
+        public static IComparer<Member> 按姓名遞減排序
+        {
+            get
+            {
+                return new CompareByNameDESC(); 
+            }
+        }
+        public static IComparer<Member> 按姓名遞增排序
+        {
+            get 
+            { 
+                return new CompareByNameASC(); 
+            }
+        }
+
+        
+        public int Height { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public int CompareTo(Member other)
+        {
+            Member x = this;
+            Member y = other;
+
+            //return y.Height.CompareTo(x.Height);
+            return y.DateOfBirth.CompareTo(x.DateOfBirth);
+
+            //if (x.Height > y.Height)
+            //    return 1000;
+            //else if (x.Height == y.Height)
+            //    return 0;
+            //else
+            //    return -999;
+        }
+
+        public override string ToString()
+        {
+            return $"Name={Name},Height={Height},Birthday={DateOfBirth:yyyy/MM/dd}";
+        }
+    }
+}
---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class A {
+        public string Name;
+        private string _Email;
+
+        public string Email
+        {
+            get { return _Email; }
+            set { _Email = value; }
+        }
+        public int Height { get; set; }//屬性的簡短寫法
+
+        //方法要有大括號
+        public int Add (int numl,int num2)
+        {
+            return numl + num2;
+        }
+    }
+    interface IB{
+        //介面裡,不能寫建構函數和field
+
+        //只能寫這個語法,不能寫public,private
+
+        string Email { get; set; }
+
+        /// <summary>
+        /// 只能這樣宣告,不能有大括號,必須接分號
+        /// </summary>
+        /// <param name="numl"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        int Add(int numl, int num2);
+    }
+    /// <summary>
+    /// 顯示可能修正,實作
+    /// </summary>
+    class Member : IB
+    {
+        //1.介面有的類別都要有 2.都只用public
+        public string Email {get; set ;}
+
+        public int Add(int numl, int num2)
+        {
+            return numl + num2;
+        }
+    }
+
+}
